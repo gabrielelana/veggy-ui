2de6443ec4faeeb98b6bea8b0ae7afc28d4dd9a5
Emanuele DelBono <emanuele@codiceplastico.com>
2016-10-13 11:20:14 +0200

    work-on-ping

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,1 +1,6 @@
-console.log('Start here...')
+import ReactDOM from 'react-dom'
+import Hello from './Hello'
+
+require('../sass/style.scss')
+
+ReactDOM.render(<Hello />, document.getElementById('app'))
