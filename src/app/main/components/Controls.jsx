3fc73bcce08b074009329033941828753c304908
Emanuele DelBono <emanuele@codiceplastico.com>
2016-10-14 16:12:07 +0200

    spread parameters

--- src/app/main/components/Controls.jsx
+++ src/app/main/components/Controls.jsx
@@ -1,12 +1,12 @@
-const Controls = props => (
+const Controls = ({startDisabled, squashDisabled, onStart, onSquash}) => (
   <div className="column">
     <div className="box">
       <div className="control is-grouped ">
         <div className="control">
-          <button id="startButton" disabled={props.startDisabled} onClick={props.onStart} className="button is-primary is-large">Start</button>
+          <button id="startButton" disabled={startDisabled} onClick={onStart} className="button is-primary is-large">Start</button>
         </div>
         <div className="control">
-          <button id="squashButton" disabled={props.squashDisabled} onClick={props.onSquash} className="button is-danger is-large">Squash</button>
+          <button id="squashButton" disabled={squashDisabled} onClick={onSquash} className="button is-danger is-large">Squash</button>
         </div>
       </div>
     </div>
