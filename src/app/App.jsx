0bc4c27e2aa491dc3a413a6d0cd1ff5d948d275b
Emanuele DelBono <emanuele@codiceplastico.com>
2016-10-18 10:05:32 +0200

    resolved conflicts

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,25 +1,11 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
-import {Route, Router, IndexRoute, hashHistory } from 'react-router'
+import {Route, Router, hashHistory } from 'react-router'
 
 require('../sass/style.scss')
 
-export const App = React.createClass({
-  render() {
-    //const children = React.cloneElement(this.props.children)
-    return (
-      <div>
-        {this.props.children}
-      </div>
-    )
-  }
-})
-
 ReactDOM.render((
   <Router history={hashHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={require('./main')} />
-      <Route path="/login" component={require('./login')} />
-    </Route>
+    <Route path="/" component={require('./main')} />
+    <Route path="/login" component={require('./login')} />
   </Router>
   ), document.getElementById('app'))
