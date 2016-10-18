53675b8cc6cf9c84efb134ebcff201e70832d2c9
Emanuele DelBono <emanuele@codiceplastico.com>
2016-10-18 10:07:45 +0200

    refactoring app

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,18 +1,6 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
 import Main from './main'
 
 require('../sass/style.scss')
-
-export const App = React.createClass({
-  render() {
-    const children = React.cloneElement(this.props.children, { store: {} })
-    return (
-      <div>
-        {children}
-      </div>
-    )
-  }
-})
 
 ReactDOM.render(<Main /> , document.getElementById('app'))
