71333a34d5194f94c91087c47abdd03973c82031
Emanuele DelBono <emanuele@codiceplastico.com>
2017-01-13 15:56:21 +0100

    modal?

--- src/app/main/components/Controls.jsx
+++ src/app/main/components/Controls.jsx
@@ -1,9 +1,11 @@
 const Controls = ({startDisabled, squashDisabled, onStart, onSquash}) => (
   <div className="column">
+    <Modal />
     <div className="box">
       <div className="control is-grouped ">
         <div className="control">
           <button id="startButton" disabled={startDisabled} onClick={onStart} className="button is-primary is-large">Start</button>
+          <input className="input" type="text" placeholder="Text input" />
         </div>
         <div className="control">
           <button id="squashButton" disabled={squashDisabled} onClick={onSquash} className="button is-danger is-large">Squash</button>
@@ -12,6 +14,27 @@ const Controls = ({startDisabled, squashDisabled, onStart, onSquash}) => (
     </div>
   </div>
 )
+
+
+const Modal = () => (
+  <div className="modal is-active">
+      <div className="modal-background"></div>
+      <div className="modal-card is-active">
+        <header className="modal-card-head">
+          <p className="modal-card-title">Modal title</p>
+          <button className="delete"></button>
+        </header>
+        <section className="modal-card-body">
+          Ciao
+        </section>
+        <footer className="modal-card-foot">
+          <a className="button is-primary">Save changes</a>
+          <a className="button">Cancel</a>
+        </footer>
+      </div>
+    </div>
+)
+
 
 
 Controls.defaultProps = {
