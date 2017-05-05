d13375dc72e1208c796700c3e024b44597d0e4c2
Emanuele DelBono <emanuele@codiceplastico.com>
2017-05-05 16:27:05 +0200

    Modularized the modal

--- src/app/main/components/LoginModal.jsx
+++ src/app/main/components/LoginModal.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Modal from './Modal'
 
 class LoginModal extends React.Component {
   constructor(props){
@@ -11,52 +12,16 @@ class LoginModal extends React.Component {
   }
   render() {
     return (
-      <div className={`modal ${this.props.isActive?'is-active':''}`}>
-        <div className="modal-background"></div>
-        <div className="modal-card">
-          <header className="modal-card-head">
-            <p className="modal-card-title">Please login...</p>
-          </header>
-          <section className="modal-card-body">
-            <div className="field">
-              <label className="label">Username</label>
-              <p className="control">
-                <input className="input" type="text" ref="username"/>
-              </p>
-            </div>
-          </section>
-          <footer className="modal-card-foot">
-            <a className="button is-success" onClick={this.handleLoginClick}>Login</a>
-            <WaitingWidget show={this.props.waiting} />
-          </footer>
+      <Modal isActive={this.props.isActive} title="Please login..." onOkClick={this.handleLoginClick} okBtnLabel="Login" waiting={this.props.waiting}>
+        <div className="field">
+          <label className="label">Username</label>
+          <p className="control">
+            <input className="input" type="text" ref="username"/>
+          </p>
         </div>
-      </div>
+      </Modal>
     )
   }
 }
-
-function WaitingWidget({show}) {
-  if (show){
-    return (
-      <div className="waiting">
-        <svg width="34px" height="34px" preserveAspectRatio="xMidYMid" className="uil-ripple">
-          <g>
-            <animate attributeName="opacity" dur="2s" repeatCount="indefinite" begin="0s" keyTimes="0;0.33;1" values="1;1;0"></animate>
-            <circle cx="20" cy="20" r="24" stroke="#1fc8db" fill="none" strokeWidth="2" strokeLinecap="round">
-              <animate attributeName="r" dur="2s" repeatCount="indefinite" begin="0s" keyTimes="0;0.33;1" values="0;8;16"></animate>
-            </circle>
-          </g>
-          <g>
-            <animate attributeName="opacity" dur="2s" repeatCount="indefinite" begin="1s" keyTimes="0;0.33;1" values="1;1;0"></animate>
-            <circle cx="20" cy="20" r="8" stroke="#ed6c63" fill="none" strokeWidth="2" strokeLinecap="round">
-              <animate attributeName="r" dur="2s" repeatCount="indefinite" begin="1s" keyTimes="0;0.33;1" values="0;8;16"></animate>
-            </circle>
-          </g>
-        </svg>
-      </div>)
-  } 
-  return null
-}
-
 
 export default LoginModal
---
+++ src/app/main/components/Modal.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+class Modal extends React.Component {
+  render() {
+    let cancelButton = null
+    if (this.props.onCancelClick) {
+      cancelButton = <a class="button">{this.props.cancelLabel}</a>
+    }
+    return (
+      <div className={`modal ${this.props.isActive?'is-active':''}`}>
+        <div className="modal-background"></div>
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <p className="modal-card-title">{this.props.title}</p>
+          </header>
+          <section className="modal-card-body">
+            {this.props.children}
+          </section>
+          <footer className="modal-card-foot">
+            <a className="button is-success" onClick={this.props.onOkClick}>{this.props.okBtnLabel}</a>
+            {cancelButton}
+            <WaitingWidget show={this.props.waiting} />
+          </footer>
+        </div>
+      </div>
+    )
+  }
+}
+
+function WaitingWidget({show}) {
+  if (show){
+    return (
+      <div className="waiting">
+        <svg width="34px" height="34px" preserveAspectRatio="xMidYMid" className="uil-ripple">
+          <g>
+            <animate attributeName="opacity" dur="2s" repeatCount="indefinite" begin="0s" keyTimes="0;0.33;1" values="1;1;0"></animate>
+            <circle cx="20" cy="20" r="24" stroke="#1fc8db" fill="none" strokeWidth="2" strokeLinecap="round">
+              <animate attributeName="r" dur="2s" repeatCount="indefinite" begin="0s" keyTimes="0;0.33;1" values="0;8;16"></animate>
+            </circle>
+          </g>
+          <g>
+            <animate attributeName="opacity" dur="2s" repeatCount="indefinite" begin="1s" keyTimes="0;0.33;1" values="1;1;0"></animate>
+            <circle cx="20" cy="20" r="8" stroke="#ed6c63" fill="none" strokeWidth="2" strokeLinecap="round">
+              <animate attributeName="r" dur="2s" repeatCount="indefinite" begin="1s" keyTimes="0;0.33;1" values="0;8;16"></animate>
+            </circle>
+          </g>
+        </svg>
+      </div>)
+  } 
+  return null
+}
+
+export default Modal
