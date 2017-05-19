f38ec80036901a38236fbc752e9d4906d96c020a
Emanuele DelBono <emanuele@codiceplastico.com>
2017-05-19 16:52:41 +0200

    refactorin timerActions

--- src/app/main/actions/timerActions.js
+++ src/app/main/actions/timerActions.js
@@ -2,21 +2,22 @@ import R from 'ramda'
 import sendCommand from '../../sendCommand'
 import settings from 'settings'
 
+const selected = u => u.selected
+const project_timer_id = u => (u.timer_id)
+
 const timerActions = {
   startPomodoro(timer_id, users, description) {
-    const isSelected = u => u.selected
-    const select = u => (u.timer_id)
-    const selectedUsers = R.pipe(R.filter(isSelected), R.map(select))(users)
-    var cmd = {command: 'StartPomodoro', duration: settings.duration, timer_id: timer_id, description: description}
-    if (selectedUsers.length > 0){
+    const selectedUsers = R.pipe(R.filter(selected), R.map(project_timer_id))(users)
+    var cmd = { command: 'StartPomodoro', duration: settings.duration, timer_id: timer_id, description: description }
+    if (selectedUsers.length > 0) {
       cmd.command = 'StartSharedPomodoro'
       cmd.shared_with = selectedUsers
     } 
     sendCommand(cmd)
   },
   squash(timer_id, pomodoro_id, isShared) {
-    var cmd = {command: 'SquashPomodoro', timer_id: timer_id, pomodoro_id: pomodoro_id}
-    if (isShared){
+    var cmd = { command: 'SquashPomodoro', timer_id: timer_id, pomodoro_id: pomodoro_id }
+    if (isShared) {
       cmd.command = 'SquashSharedPomodoro'
     }
     sendCommand(cmd)
